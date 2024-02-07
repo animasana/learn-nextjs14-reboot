27871faf8f0471af37436136b91c3b33d6cdb36d
animasana <animagig@gmail.com>
2024-02-08 01:08:20 +0900

    +Suspense in Similar

--- app/(movies)/movies/[id]/similar/page.tsx
+++ app/(movies)/movies/[id]/similar/page.tsx
@@ -1,42 +1,15 @@
-import Link from "next/link";
-import Movie from "../../../../../components/movie";
-import styles from "./similar-movies.module.css";
-import { API_URL, MOVIE_NOT_FOUND } from "../../../../constants";
-
-const getSimilarMovies = async (id: string) => {  
-  const response = await fetch(`${API_URL}/${id}/similar`);
-  return response.json();  
-}
+import { Suspense } from "react";
+import SimilarMovies from "../../../../../components/similar-movies";
 
 interface IParams {
   params: { id: string }; 
 }
 
-export default async function SimilarMovies({ params: { id } }: IParams) {
-  const similarMovies = await getSimilarMovies(id);
+export default async function Similar({ params: { id } }: IParams) {
   return (
-    <>       
-      <span className={styles.anchor_back}>        
-        <Link prefetch href={`/movies/${id}`}>&larr; Back to the movie info</Link>
-      </span>      
-      { 
-        (similarMovies.length !== 0) ?
-        <div className={styles.container}>         
-          {
-            similarMovies.map(movie => 
-              <Movie 
-                key={movie.id} 
-                id={movie.id} 
-                title={movie.title} 
-                poster_path={movie.poster_path ?? MOVIE_NOT_FOUND} 
-              />            
-            ) 
-          }
-        </div> 
-        : 
-        <h1 className={styles.not_found}>Movie Not Found!!!</h1>          
-      }      
-    </>
+    <Suspense fallback={<h1>Loading Similar Movies...</h1>}>
+      <SimilarMovies id={id} />
+    </Suspense>
   );
 }
 
---
+++ components/similar-movies.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import { API_URL, MOVIE_NOT_FOUND } from "../app/constants";
+import Movie from "./movie";
+import styles from "../styles/similar-movies.module.css";
+
+const getSimilarMovies = async (id: string) => {  
+  const response = await fetch(`${API_URL}/${id}/similar`);
+  return response.json();  
+}
+
+export default async function SimilarMovies({ id }: { id: string }) {
+  const similarMovies = await getSimilarMovies(id);
+  return (
+    <>       
+      <span className={styles.anchor_back}>        
+        <Link prefetch href={`/movies/${id}`}>&larr; Back to the movie info</Link>
+      </span>      
+      { 
+        (similarMovies.length !== 0) ?
+        <div className={styles.container}>         
+          {
+            similarMovies.map(movie => 
+              <Movie 
+                key={movie.id} 
+                id={movie.id} 
+                title={movie.title} 
+                poster_path={movie.poster_path ?? MOVIE_NOT_FOUND} 
+              />            
+            ) 
+          }
+        </div> 
+        : 
+        <h1 className={styles.not_found}>Movie Not Found!!!</h1>          
+      }      
+    </>
+  );
+}
